647dba21c4b0a9592d5ae17357e97b704b044cd4
Zhuk <mega_zhuk7777@ukr.net>
2020-09-30 08:56:38 +0300

    module-12

--- src/index.js
+++ src/index.js
@@ -1,56 +1,114 @@
-import './scss/main.scss';
-import menues from './json/menu.json';
-import menuTemplate from './templates/menu-tamplete.hbs';
+'use strick';
 
-const menuItem = {
-  jsMenu: document.querySelector('.js-menu'),
+import './index.html';
+import './scss/main.scss'; // если собираем не через ./src/scss/main.scss
+import cardCountry from './templates/card_country.hbs';
+import listCountry from './templates/list_country.hbs';
+import templateError from './templates/error.hbs';
+//npm i --save lodash.debounce
+const input = document.querySelector('#js-name_country');
+const card = document.querySelector('.card');
+
+//----------------------pnotify
+import { success, error, defaults, Stack } from '@pnotify/core';
+defaults.width = '300px';
+
+//-------------------- word lodash
+const _ = require('../node_modules/lodash'); //подсоединили библиотеку lodash
+
+// const text = _.debounce(() => {
+// 	console.log(66);
+// }, 5556);
+// input.addEventListener('click', (e) => text());
+// -----------------
+//https://github.com/sciactive/pnotify#styles
+//https://codesandbox.io/s/pnotify-5-in-vanilla-es6-x4ipu?file=/src/index.js
+//https://sciactive.com/pnotify/
+//https://www.jqueryscript.net/other/JS-Notification-Plugin-For-Bootstrap-jQuery-UI-Pines-Notify.html
+// --------------------
+
+input.addEventListener('input', (e) => _.debounce(dataCountry(e), 500));
+
+const dataCountry = function(e) {
+	//e.preventDefault();
+	e.stopPropagation();
+	const eve = e.target;
+
+	fetch(`https://restcountries.eu/rest/v2/name/${eve.value}`, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json'
+		}
+	})
+		.then((response) => {
+			if (eve.nextSibling.nextElementSibling) {
+				eve.nextSibling.nextElementSibling.remove();
+			}
+			if (!response.ok) {
+				myPnotify(0);
+				return;
+			}
+			return response.json();
+		})
+		.then((data) => {
+			if (data.length > 1) {
+				addItems(assemblyLi(data));
+				return;
+			}
+
+			addItems(assemblyCountry(data));
+		})
+		.catch(() => {
+			addItems(templateError());
+		});
 };
 
-function buildMenu(menues) {
-  const murkup = menues.map(menu => menuTemplate(menu)).join('');
-  menuItem.jsMenu.insertAdjacentHTML('beforeend', murkup);
-}
-
-buildMenu(menues);
-
-const refs = {
-  body: document.querySelector('body'),
-  menu: document.querySelector('#menu'),
-  themeSwitch: document.querySelector('#theme-switch-control'),
-  loc: {
-    theme: localStorage.getItem('theme'),
-  },
+const addItems = (item) => {
+	card.insertAdjacentHTML('beforeend', item);
 };
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
+const assemblyCountry = (data) => {
+	myPnotify();
+	return data.map((data) => createTemplateCountry(data));
 };
 
-const lightTheme = tag => {
-  tag.classList.remove(Theme.DARK);
-  tag.classList.add(Theme.LIGHT);
-  localStorage.setItem('theme', Theme.LIGHT);
+const assemblyLi = (data) => {
+	myPnotify();
+	return createTemplateLi({ name: data.map(({ name }) => name) });
 };
 
-
-const darkTheme = tag => {
-  tag.classList.remove(Theme.LIGHT);
-  tag.classList.add(Theme.DARK);
-  localStorage.setItem('theme', Theme.DARK);
+const createTemplateCountry = (items) => {
+	return cardCountry(items);
 };
-const applyTheme = boolean => {
-  boolean ? darkTheme(refs.body) : lightTheme(refs.body);
+const createTemplateLi = (items) => {
+	return listCountry(items);
 };
 
-if (refs.loc.theme === Theme.DARK) {
-  darkTheme(refs.body);
-  refs.themeSwitch.checked = true;
-}
-
-refs.body.addEventListener('click', e => {
-  if (e.target.nodeName !== 'INPUT') {
-    return;
-  }
-  applyTheme(e.target.checked);
-});
+const myPnotify = (a) => {
+	const myStackSuccess = new Stack({
+		dir1: 'down',
+		dir2: 'right',
+		firstpos1: 25,
+		firstpos2: 125
+	});
+	const myStackError = new Stack({
+		dir1: 'down',
+		dir2: 'left',
+		firstpos1: 25,
+		firstpos2: 125
+	});
+	if (a === 0) {
+		return error({
+			title: 'Не найдено',
+			text: 'Что-то пошло не так и страна не найдена',
+			delay: 1000,
+			stack: myStackError
+		});
+	}
+	return success({
+		title: 'Найдено',
+		text: 'Найдена одна страна по вашему запросу  ',
+		delay: 1000,
+		stack: myStackSuccess
+	});
+};
